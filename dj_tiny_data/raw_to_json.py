99a33c9657af7dc54751e637c54ad23b01182eed
Tribhuvanesh Orekondy <tribhuvanesh@gmail.com>
2014-10-17 17:04:37 +0200

    Adding template for data generator

---
+++ dj_tiny_data/raw_to_json.py
@@ -0,0 +1,19 @@
+"""
+Given:
+1. Last.fm Dataset
+2. Million Song Dataset
+3. Lexicon for lyrics
+Generates a file, each line containing the json:
+{
+    "id" : "TRAAAFD128F92F423A",
+    "wordcount": { <wordid> : <normalized_count>, ...},
+    "genre": [ "genre1", "genre2", .. ]
+}
+"""
+import json
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
