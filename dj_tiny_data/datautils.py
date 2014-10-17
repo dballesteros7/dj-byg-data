4203393a15a2b9e020ab8af281f946dcd9baf089
Tribhuvanesh Orekondy <tribhuvanesh@gmail.com>
2014-10-17 16:21:14 +0200

    Adding Tag to Genre adapter

---
+++ dj_tiny_data/datautils.py
@@ -0,0 +1,11 @@
+"""
+A bunch of commonly used helper functions
+"""
+
+def tag_to_genre(tag):
+    """
+    Given one of the last.fm tags defined in $PROJECT_HOME/data/lastfm-tags.txt,
+       returns one of the genres listed in $PROJECT_HOME/data/lastfm-genres.txt.
+       Returns None if tag is unrecognized.
+    """
+    return tag
