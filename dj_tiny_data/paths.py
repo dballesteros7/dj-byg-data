243f814f6d10b25469ccd62dc804e233938f693d
Diego A. Ballesteros Villamizar <diegob@student.ethz.ch>
2014-10-28 19:54:12 +0100

    Update

--- dj_tiny_data/paths.py
+++ dj_tiny_data/paths.py
@@ -13,3 +13,5 @@ OUTPUT_FILE = os.path.join(DATA_DIR, 'tag_track_data.txt')
 TF_IDF_MATRIX_PATH = os.path.join(DATA_DIR, 'tf_idf_matrix.txt')
 WORD_LIST_PATH = os.path.join(DATA_DIR, 'word_list.txt')
 LSH_CLUSTER_FILE = os.path.join(DATA_DIR, 'lsh_cluster_file.txt')
+
+K_MEANS_PERF_PATH = os.path.join(DATA_DIR, 'kmeans_perf.txt')
--- dj_tiny_data/tinykmeans.py
+++ dj_tiny_data/tinykmeans.py
@@ -1,13 +1,16 @@
 import sys
 
-from dj_tiny_data import kmeans, quality_metrics, tfidf
+from dj_tiny_data import kmeans, paths, quality_metrics, tfidf
 
 
 def main():
     tfidf.calculate_tf_idf_matrix()
-    result = kmeans.k_means()
     qm = quality_metrics.QualityMetrics()
-    qm.evaluate_kmeans(result)
+    with open(paths.K_MEANS_PERF_PATH, 'w') as foutput:
+        for n in xrange(5, 101):
+            result = kmeans.k_means(n_clusters=n)
+            quality = qm.evaluate_kmeans(result)
+            foutput.write('{}, {}\n'.format(str(n), str(quality)))
 
 if __name__ == '__main__':
     sys.exit(main())
