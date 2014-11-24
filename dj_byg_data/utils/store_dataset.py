f8ec5b4c3b6edce26487f0d0d45e2d71ee1613aa
Diego A. Ballesteros Villamizar <diegob@student.ethz.ch>
2014-11-24 03:02:22 +0100

    Temp commit - 2

--- dj_byg_data/utils/store_dataset.py
+++ dj_byg_data/utils/store_dataset.py
@@ -1,96 +1,124 @@
 """Tool to upload the dataset to Amazon S3."""
-
-from boto.s3 import connect_to_region
-from boto.s3.connection import Location
-from boto.s3.key import Key
-
 import json
-import sys
-import shutil
-import h5py
-import tarfile
-import tempfile
 import glob
 import os
-import zipfile
+import shutil
+import sys
+import tarfile
+import tempfile
+import time
+import traceback
 
-DATA_DIR = '/local/diegob/workspace/bigdata-2014/dj-byg-data/data/'
-S3_BUCKET = 'dj-byg-data'
-DATA_PREFIX = 'songs_data/'
+import h5py
+
+from dj_byg_data import DATA_PATH
+from dj_byg_data.model.connect import DBConnection
+from dj_byg_data.model.schema import (songs, artists, artist_terms,
+                                      song_artist_terms)
 
 
 def load_tracks_with_lyrics():
     tracks_with_lyrics = set()
-    with open(os.path.join(DATA_DIR, 'tracks_with_lyrics.txt'), 'r') as lyrics:
+    with open(os.path.join(
+            DATA_PATH, 'tracks_with_lyrics.txt'), 'r') as lyrics:
         for line in lyrics:
             tracks_with_lyrics.add(line.strip())
     return tracks_with_lyrics
 
 
-def load_zipfiles():
-    zipfile_train = zipfile.ZipFile(
-        os.path.join(DATA_DIR, 'lastfm_train.zip'), 'r')
-    zipfile_test = zipfile.ZipFile(
-        os.path.join(DATA_DIR, 'lastfm_test.zip'), 'r')
-    return zipfile_train, zipfile_test
-
-
 def main():
-    s3_connection = connect_to_region('eu-west-1')
-    bucket = s3_connection.lookup(S3_BUCKET)
-    if bucket is None:
-        bucket = s3_connection.create_bucket(S3_BUCKET, location=Location.EU)
+    db_conn = DBConnection()
+    engine = db_conn.engine
     tracks_with_lyrics = load_tracks_with_lyrics()
-    zipfile_train, zipfile_test = load_zipfiles()
-    tar_files = glob.glob(DATA_DIR + 'MillionSongDataset/[A-Z].tar.gz')
-    for tar_file_path in tar_files:
+    tar_files = glob.glob(os.path.join(DATA_PATH,
+                                       'MillionSongDataset', '[A-Z].tar.gz'))
+    lastfm_tar_files = glob.glob(os.path.join(DATA_PATH,
+                                              'MillionSongDataset',
+                                              '[A-Z].lastfm.tar.gz'))
+    tar_files.sort()
+    lastfm_tar_files.sort()
+    counter = 0
+    start_time = time.time()
+    for tar_file_path, lastfm_tar_file_path in zip(tar_files,
+                                                   lastfm_tar_files):
         tar_file = tarfile.open(tar_file_path, 'r')
+        lastfm_tar_file = tarfile.open(lastfm_tar_file_path, 'r')
         next_member = tar_file.next()
         while next_member is not None:
             if next_member.isfile() and os.path.splitext(os.path.basename(
                     next_member.name))[0] in tracks_with_lyrics:
-                lastfm_file_train = os.path.join(
-                    'lastfm_train', os.path.splitext(next_member.name)[0] +
-                    '.json')
-                lastfm_file_test = os.path.join(
-                    'lastfm_test', os.path.splitext(next_member.name)[0] +
-                    '.json')
-                try:
-                    lastfm_file = zipfile_train.open(lastfm_file_train, 'r')
-                except KeyError:
-                    try:
-                        lastfm_file = zipfile_test.open(lastfm_file_test, 'r')
-                    except KeyError:
-                        lastfm_data = {'tags': [], 'similars': []}
-                    else:
-                        lastfm_data = json.load(lastfm_file)
-                else:
-                    lastfm_data = json.load(lastfm_file)
-                lastfm_file.close()
                 dir_path = tempfile.mkdtemp()
                 tar_file.extract(next_member, path=dir_path)
+                lastfm_member_name = os.path.splitext(
+                    next_member.name)[0] + '.json'
+                try:
+                    lastfm_file = lastfm_tar_file.extractfile(
+                        lastfm_member_name)
+                except KeyError:
+                    lastfm_data = {'tags': []}
+                else:
+                    lastfm_data = json.load(lastfm_file)
                 h5_file = h5py.File(
                     os.path.join(dir_path, next_member.name), 'r')
                 song_data = {
+                    'song_id': h5_file['metadata']['songs']['song_id'][0],
                     'artist_name':
                     h5_file['metadata']['songs']['artist_name'][0],
                     'release': h5_file['metadata']['songs']['release'][0],
                     'title': h5_file['metadata']['songs']['title'][0],
                     'artist_id': h5_file['metadata']['songs']['artist_id'][0],
                     'artist_terms':
-                    list(h5_file['metadata']['artist_terms']),
-                    'tags': lastfm_data['tags'],
-                    'similar_songs': lastfm_data['similars']
+                    list(h5_file['metadata']['artist_terms'])
                 }
-                key = Key(bucket)
-                key.key = DATA_PREFIX + h5_file[
-                    'metadata']['songs']['song_id'][0]
-                key.set_contents_from_string(json.dumps(song_data))
+                conn = engine.connect()
+                trans = conn.begin()
+                try:
+                    artist_check_stmt = artists.select().where(
+                        artists.c.artist_id == song_data['artist_id'])
+                    result = conn.execute(artist_check_stmt)
+                    if result.fetchone() is None:
+                        artist_stmt = artists.insert().values(
+                            artist_id=song_data['artist_id'],
+                            artist_name=song_data['artist_name'])
+                        conn.execute(artist_stmt)
+                    else:
+                        result.close()
+                    insert_song_stmt = songs.insert().values(
+                        song_id=song_data['song_id'],
+                        title=song_data['title'],
+                        release=song_data['release'],
+                        artist_id=song_data['artist_id'],
+                        lastfm_tags=[tag[0] for tag in lastfm_data['tags']])
+                    conn.execute(insert_song_stmt)
+                    for term in song_data['artist_terms']:
+                        term_check_stmt = artist_terms.select().where(
+                            artist_terms.c.term == term)
+                        result = conn.execute(term_check_stmt)
+                        if result.fetchone() is None:
+                            term_stmt = artist_terms.insert().values(
+                                term=term)
+                            conn.execute(term_stmt)
+                        else:
+                            result.close()
+                        insert_term_song_stmt = song_artist_terms.insert(
+                            ).values(song_id=song_data['song_id'],
+                                     term=term)
+                        conn.execute(insert_term_song_stmt)
+                except:
+                    traceback.print_exc()
+                    trans.rollback()
+                else:
+                    trans.commit()
+                    counter += 1
+                    if counter % 5000 == 0:
+                        print counter
+                        print '%s minutes' % str((time.time() - start_time)/60)
+                finally:
+                    conn.close()
                 h5_file.close()
                 shutil.rmtree(dir_path)
             next_member = tar_file.next()
         tar_file.close()
-    zipfile_train.close()
-    zipfile_test.close()
+
 if __name__ == '__main__':
     sys.exit(main())
